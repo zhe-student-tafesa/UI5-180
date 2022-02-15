1d079bae827bc34bcccda28082c0e954f8dbefc9
Frank Zhang <frank.zhang.sa.au@gmail.com>
2022-02-15 01:38:33 +0000

    first commit-finished table

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -11,6 +11,54 @@ sap.ui.define([
 
         return Controller.extend("T180.fiorichallenge.controller.Main", {
             onInit: function () {
+                var oTable = this.getView().byId("ReviewsTable");
+
+                // bind data
+                oTable.bindItems({
+                    path: "AssetReviewModel>/Reviews",
+                    template: new sap.m.ColumnListItem({
+                        // type : navigation
+                        cells: [
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>AssetName}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>Suitability}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>SuitabilityComment}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>Value}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>ValueComment}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>Durability}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>DurabilityComment}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>Longevity}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>LongevityComment}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>SummaryComments}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>SubmissionDate}"
+                            }),
+                            new sap.m.Text({
+                                text: "{AssetReviewModel>SubmittedBy}"
+                            })
+    
+                        ]
+                    })
+                });
 
             },
 
@@ -21,6 +69,20 @@ sap.ui.define([
 
                 // Example; setting the 'CurrentDate' property in the Asset Review model
                 this.getView().getModel("AssetReviewModel").setProperty("/CurrentDate", new Date());
+                // Calculate how many entries are in the ‘Reviews’ array of the ‘AssetReviewModel’, and display it on the page.
+                var myReviews= this.getView().getModel("AssetReviewModel").getProperty("/Reviews");
+                var reviewsLength=myReviews.length;
+                //bound to the XML view
+                var oData = {
+                    recipient: {
+                        Length: reviewsLength
+                    }
+                };
+                var oModel = new JSONModel(oData);
+                this.getView().setModel(oModel);
+                    
+                console.log("123");
+
             }
         });
     });
